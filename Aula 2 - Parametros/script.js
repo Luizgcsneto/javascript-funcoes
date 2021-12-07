da2ab9dbf89f093dbbcbe010984fb4319edd3257
Luiz Neto <luizgonzaga15@gmail.com>
2021-12-06 22:15:47 -0300

    aprendendo a usar o rest

--- Aula 2 - Parametros/script.js
+++ Aula 2 - Parametros/script.js
@@ -5,3 +5,11 @@ function sum(x,y,z){
 const numbers = [1,2,3]
 
 console.log(sum(...numbers))
+
+
+function confereTamanho(...args){
+    console.log(args.length)
+}
+
+confereTamanho()
+confereTamanho(1,1,22,22,3)
